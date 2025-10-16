0945e754f01046ce499d3b1a553144d349e9575b
Alexandre Lima <alexandrecurylima@gmail.com>
2025-10-16 14:11:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6845893399-245eb",
+  "appId": "1:148237168625:web:94244182f99f85ec5d1e50",
+  "apiKey": "AIzaSyB_BrL2XuhXv-v_kM77Sy6j9rzBXaCDmVs",
+  "authDomain": "studio-6845893399-245eb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "148237168625"
+};
